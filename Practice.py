536945cbf9263ca955ecfe5feebccbe63ea0d469
Jedidiah <jedidiahinyang@gmail.com>
2023-03-17 16:12:49 +0100

    moved the readme thing to the main folder and stuff like that

--- Practice.py
+++ Practice.py
@@ -1,9 +1,11 @@
 user_age = input("How old are you?: ")
-if a < 18:
+a = user_age
+b = int(a)
+if b < 18:
     print("Your ticket costs $2")
-elif a >= 18:
+elif b >= 18:
     print("Your tcket costs $10")
-elif a > 60:
+elif b > 60:
     print("Your ticket costs $5")
 else:
     print("Your ticket costs $10")
