0b51a352d72ef44f09ad5021b7d18de265e07bd9
ManLikeJedi <68284112+ManLikeJedi@users.noreply.github.com>
2023-03-17 15:35:56 +0100

    created a new  readme file

---
+++ Practice.py
@@ -0,0 +1,9 @@
+user_age = input("How old are you?: ")
+if a < 18:
+    print("Your ticket costs $2")
+elif a >= 18:
+    print("Your tcket costs $10")
+elif a > 60:
+    print("Your ticket costs $5")
+else:
+    print("Your ticket costs $10")
---
+++ test.py
@@ -0,0 +1,6 @@
+set_1 = set
+set_1.add('a')
+set_1.add('b')
+set_1.add('c')
+x = len(set)
+print(x)
