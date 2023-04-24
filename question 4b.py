9f7c25bbc0f1dff3ea65ca94d7d4c5b49f42cbe9
Jedidiah <jedidiahinyang@gmail.com>
2023-04-24 19:39:34 +0100

    made changes to the signal of X

--- question 4b.py
+++ question 4b.py
@@ -19,7 +19,7 @@ def my_ifft(X):
     return Y.conj()
 
 # to define out input signal of X
-X = np.array([1, 2, 3, 4])
+X = np.array([21, 22, 23, 24])
 
 # Compute inverse DFT of X using our own implementation
 x = my_ifft(X)
